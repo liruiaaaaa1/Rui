d7d5dd2eb21dd2bab503e6db25f9082902dfdacb
Li, Rui (Rui) <rui.li@cienet.com.cn>
2018-09-10 12:37:37 +0800

    modify

--- decorator.py
+++ decorator.py
@@ -1,13 +1,18 @@
-#装饰器
-def log（func）
-    def inner（*args,**kwargs）:
-	    print"log is：%s%s"%（args，kwargs）
-		return func（*args，**kwargs）
-	return inner
-	
+# -*- coding: UTF-8 -*-  
+#decorator
+def log(func):
+    def inner(*args,**kwargs):
+            print"log is:%s%s"%(args,kwargs)
+            return func(*args,**kwargs)
+    return inner
+    
 @log
-def func1(x,y)
-    return x+y
+def func1(x,y):
+    print(x+y)
 @log
-def func2(x,y)
-    return x*y
+def func2(x,y):
+    print(x*y)
+def main():
+    func1(5,2)
+    func2(3,4)
+main()
